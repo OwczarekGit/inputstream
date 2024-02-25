77bf7ac6bfe91df96b1f9fa1fc8e8055a317f717
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2024-02-25 15:05:10 +0100

    Implement gamepad, requires verification if it actually works as expected.

--- inputstream/src/event_handlers/linux/gamepad.rs
+++ inputstream/src/event_handlers/linux/gamepad.rs
@@ -1,31 +1,82 @@
-use std::{thread, time::Duration};
-
 use evdev::{
     uinput::{VirtualDevice, VirtualDeviceBuilder},
     AbsInfo, AbsoluteAxisType, AttributeSet, BusType, EventType, InputEvent, InputId, Key,
     UinputAbsSetup,
 };
-use lib_inputstream::{consts::GAMEPAD_DEVICE_NAME, event::gamepad::GamepadEvent};
+use lib_inputstream::{
+    consts::GAMEPAD_DEVICE_NAME,
+    event::{difference::Difference, gamepad::GamepadEvent},
+};
 
 use crate::event_handlers::handler::{EventHandler, GamepadEventHandler};
 
 impl EventHandler<GamepadEvent> for GamepadEventHandler {
     fn listen(&self, receiver: std::sync::mpsc::Receiver<GamepadEvent>) -> crate::Result<()> {
         let mut device = create_device()?;
-        thread::sleep(Duration::from_secs(10));
 
-        let res = device.emit(&[InputEvent::new(
-            EventType::KEY,
-            Key::BTN_TRIGGER_HAPPY1.code(),
-            1,
-        )]);
-
-        dbg!(res);
+        let mut gamepad_state = GamepadEvent::default();
 
         loop {
-            thread::sleep(Duration::from_secs(10));
-            if let Ok(_msg) = receiver.recv() {
-                let _ = device.emit(&[]);
+            if let Ok(msg) = receiver.recv() {
+                let mut ev = vec![];
+
+                let (l_stick, r_stick, triggers, buttons) = gamepad_state.get_diff(&msg);
+
+                if let Some(x) = l_stick.0 {
+                    ev.push(InputEvent::new(
+                        EventType::ABSOLUTE,
+                        AbsoluteAxisType::ABS_X.0,
+                        x as i32,
+                    ));
+                }
+                if let Some(y) = l_stick.1 {
+                    ev.push(InputEvent::new(
+                        EventType::ABSOLUTE,
+                        AbsoluteAxisType::ABS_Y.0,
+                        y as i32,
+                    ));
+                }
+
+                if let Some(x) = r_stick.0 {
+                    ev.push(InputEvent::new(
+                        EventType::ABSOLUTE,
+                        AbsoluteAxisType::ABS_RX.0,
+                        x as i32,
+                    ));
+                }
+                if let Some(y) = r_stick.1 {
+                    ev.push(InputEvent::new(
+                        EventType::ABSOLUTE,
+                        AbsoluteAxisType::ABS_RY.0,
+                        y as i32,
+                    ));
+                }
+
+                if let Some(l) = triggers.0 {
+                    ev.push(InputEvent::new(
+                        EventType::ABSOLUTE,
+                        AbsoluteAxisType::ABS_Z.0,
+                        l as i32,
+                    ));
+                }
+                if let Some(r) = triggers.1 {
+                    ev.push(InputEvent::new(
+                        EventType::ABSOLUTE,
+                        AbsoluteAxisType::ABS_RZ.0,
+                        r as i32,
+                    ));
+                }
+
+                for (state, btn) in buttons {
+                    ev.push(InputEvent::new(
+                        EventType::KEY,
+                        Key::from(btn).code(),
+                        state.into(),
+                    ));
+                }
+
+                gamepad_state = msg;
+                let _ = device.emit(&ev);
             }
         }
     }
@@ -33,53 +84,79 @@ impl EventHandler<GamepadEvent> for GamepadEventHandler {
 
 fn create_device() -> crate::Result<VirtualDevice> {
     let mut buttons = AttributeSet::new();
-    /* buttons.insert(Key::BTN_0);
-    buttons.insert(Key::BTN_1);
-    buttons.insert(Key::BTN_2);
-    buttons.insert(Key::BTN_3);
-    buttons.insert(Key::BTN_4);
-    buttons.insert(Key::BTN_5);
-    buttons.insert(Key::BTN_6);
-    buttons.insert(Key::BTN_7);
-    buttons.insert(Key::BTN_8);
-    buttons.insert(Key::BTN_9);
-    buttons.insert(Key::BTN_DPAD_UP);
-    buttons.insert(Key::BTN_DPAD_DOWN);
-    buttons.insert(Key::BTN_DPAD_LEFT);
-    buttons.insert(Key::BTN_DPAD_RIGHT);
-    buttons.insert(Key::BTN_THUMB);
-    buttons.insert(Key::BTN_THUMB2);
-    buttons.insert(Key::BTN_START);
+    buttons.insert(Key::BTN_NORTH);
+    buttons.insert(Key::BTN_SOUTH);
+    buttons.insert(Key::BTN_WEST);
+    buttons.insert(Key::BTN_EAST);
+    buttons.insert(Key::BTN_TL);
+    buttons.insert(Key::BTN_TR);
+
     buttons.insert(Key::BTN_SELECT);
-    buttons.insert(Key::BTN_LEFT);
-    buttons.insert(Key::BTN_RIGHT);
-    buttons.insert(Key::BTN_EXTRA); */
+    buttons.insert(Key::BTN_START);
+    buttons.insert(Key::BTN_MODE);
+    buttons.insert(Key::KEY_MUTE);
+
+    buttons.insert(Key::BTN_THUMBL);
+    buttons.insert(Key::BTN_THUMBR);
+
     buttons.insert(Key::BTN_TRIGGER_HAPPY1);
     buttons.insert(Key::BTN_TRIGGER_HAPPY2);
     buttons.insert(Key::BTN_TRIGGER_HAPPY3);
     buttons.insert(Key::BTN_TRIGGER_HAPPY4);
 
+    const MIN: i32 = -32768;
+    const MAX: i32 = 32767;
+    const FUZZ: i32 = 16;
+    const FLAT: i32 = 128;
+    const RES: i32 = 1;
+
+    const Z_MIN: i32 = 0;
+    const Z_MAX: i32 = 255;
+    const Z_RES: i32 = 1;
+    const Z_FUZZ: i32 = 1;
+    const Z_FLAT: i32 = 1;
+
     let x_axis = UinputAbsSetup::new(
         AbsoluteAxisType::ABS_X,
-        AbsInfo::new(0, -32767, 32767, 20, 20, 128),
+        AbsInfo::new(0, MIN, MAX, FUZZ, FLAT, RES),
     );
-
     let y_axis = UinputAbsSetup::new(
         AbsoluteAxisType::ABS_Y,
-        AbsInfo::new(0, -32767, 32767, 20, 20, 1),
+        AbsInfo::new(0, MIN, MAX, FUZZ, FLAT, RES),
+    );
+
+    let rx_axis = UinputAbsSetup::new(
+        AbsoluteAxisType::ABS_RX,
+        AbsInfo::new(0, MIN, MAX, FUZZ, FLAT, RES),
+    );
+    let ry_axis = UinputAbsSetup::new(
+        AbsoluteAxisType::ABS_RY,
+        AbsInfo::new(0, MIN, MAX, FUZZ, FLAT, RES),
     );
 
     let z_axis = UinputAbsSetup::new(
         AbsoluteAxisType::ABS_Z,
-        AbsInfo::new(0, -32767, 32767, 20, 20, 128),
+        AbsInfo::new(0, Z_MIN, Z_MAX, Z_FUZZ, Z_FLAT, Z_RES),
     );
+    let rz_axis = UinputAbsSetup::new(
+        AbsoluteAxisType::ABS_RZ,
+        AbsInfo::new(0, Z_MIN, Z_MAX, Z_FUZZ, Z_FLAT, Z_RES),
+    );
+
+    let hat_x = UinputAbsSetup::new(AbsoluteAxisType::ABS_HAT0X, AbsInfo::new(0, -1, 1, 1, 1, 1));
+    let hat_y = UinputAbsSetup::new(AbsoluteAxisType::ABS_HAT0Y, AbsInfo::new(0, -1, 1, 1, 1, 1));
 
     Ok(VirtualDeviceBuilder::new()?
         .name(GAMEPAD_DEVICE_NAME)
-        .input_id(InputId::new(BusType::BUS_USB, 0x045E, 0x02EA, 0))
+        .input_id(InputId::new(BusType::BUS_USB, 0x45E, 0x2A1, 0x100))
         .with_keys(&buttons)?
+        .with_absolute_axis(&hat_x)?
+        .with_absolute_axis(&hat_y)?
         .with_absolute_axis(&x_axis)?
         .with_absolute_axis(&y_axis)?
+        .with_absolute_axis(&rx_axis)?
+        .with_absolute_axis(&ry_axis)?
         .with_absolute_axis(&z_axis)?
+        .with_absolute_axis(&rz_axis)?
         .build()?)
 }
--- lib-inputstream/src/event/gamepad.rs
+++ lib-inputstream/src/event/gamepad.rs
@@ -3,6 +3,8 @@ use std::{
     str::{FromStr, Split},
 };
 
+use super::difference::Difference;
+
 #[derive(Debug, Default, Clone, Copy, PartialEq, PartialOrd)]
 pub struct GamepadEvent {
     pub left_stick: (f32, f32),
@@ -74,52 +76,104 @@ impl FromStr for GamepadEvent {
     }
 }
 
+impl Difference for GamepadEvent {
+    type Output = (
+        (Option<f32>, Option<f32>),
+        (Option<f32>, Option<f32>),
+        (Option<f32>, Option<f32>),
+        Vec<(bool, GamepadButton)>,
+    );
+
+    fn get_diff(&self, other: &Self) -> Self::Output {
+        let motion_diff = |val: f32, new: f32| val.ne(&new).then_some(new);
+
+        let add_button_change = |v: &mut Vec<(bool, GamepadButton)>, btn: GamepadButton| {
+            if (self.buttons & btn as u32) != (other.buttons & btn as u32) {
+                v.push((other.buttons & btn as u32 > 0, btn));
+            }
+        };
+
+        let mut buttons = vec![];
+
+        add_button_change(&mut buttons, GamepadButton::A);
+        add_button_change(&mut buttons, GamepadButton::B);
+        add_button_change(&mut buttons, GamepadButton::Y);
+        add_button_change(&mut buttons, GamepadButton::X);
+        add_button_change(&mut buttons, GamepadButton::Down);
+        add_button_change(&mut buttons, GamepadButton::Up);
+        add_button_change(&mut buttons, GamepadButton::Left);
+        add_button_change(&mut buttons, GamepadButton::Right);
+        add_button_change(&mut buttons, GamepadButton::Select);
+        add_button_change(&mut buttons, GamepadButton::Select);
+        add_button_change(&mut buttons, GamepadButton::Mode);
+        add_button_change(&mut buttons, GamepadButton::BumperLeft);
+        add_button_change(&mut buttons, GamepadButton::BumperRight);
+        add_button_change(&mut buttons, GamepadButton::StickLeft);
+        add_button_change(&mut buttons, GamepadButton::StickRight);
+
+        let diff_left = (
+            motion_diff(self.left_stick.0, other.left_stick.0),
+            motion_diff(self.left_stick.1, other.left_stick.1),
+        );
+
+        let diff_right = (
+            motion_diff(self.right_stick.0, other.right_stick.0),
+            motion_diff(self.right_stick.1, other.right_stick.1),
+        );
+
+        let diff_trigger = (
+            motion_diff(self.triggers.0, other.triggers.0),
+            motion_diff(self.triggers.1, other.triggers.1),
+        );
+
+        (diff_left, diff_right, diff_trigger, buttons)
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 #[repr(u32)]
-enum GamepadButton {
-    X = 1 << 0,
-    Circle = 1 << 1,
-    Triangle = 1 << 2,
-    Square = 1 << 3,
+pub enum GamepadButton {
+    A = 1 << 0,
+    B = 1 << 1,
+    Y = 1 << 2,
+    X = 1 << 3,
     Down = 1 << 4,
     Right = 1 << 5,
     Up = 1 << 6,
     Left = 1 << 7,
-    Options = 1 << 8,
-    Create = 1 << 9,
-    Ps = 1 << 10,
-    Mute = 1 << 11,
-    BumperLeft = 1 << 12,
-    BumperRight = 1 << 13,
-    StickLeft = 1 << 14,
-    StickRight = 1 << 15,
-    Touchpad = 1 << 16,
+    Start = 1 << 8,
+    Select = 1 << 9,
+    Mode = 1 << 10,
+    BumperLeft = 1 << 11,
+    BumperRight = 1 << 12,
+    StickLeft = 1 << 13,
+    StickRight = 1 << 14,
 }
 
+/// This maps XBOX 360 Controller.
+/// Theese are different for different controllers.
+/// For many you will need to use HAT absolute events for DPAD
+/// X360 also sends TRIGGER_HAPPY key events for DPAD.
 #[cfg(target_os = "linux")]
 impl From<GamepadButton> for evdev::Key {
     fn from(value: GamepadButton) -> Self {
         use evdev::Key;
         match value {
-            // I'm not sure what is going on here.
-            // I'm very likely doing something wrong.
-            GamepadButton::X => todo!(),
-            GamepadButton::Circle => Key::BTN_THUMB2,
-            GamepadButton::Triangle => Key::BTN_START,
-            GamepadButton::Square => Key::BTN_THUMB,
-            GamepadButton::Down => todo!(),
-            GamepadButton::Right => todo!(),
-            GamepadButton::Up => todo!(),
-            GamepadButton::Left => Key::BTN_LEFT,
-            GamepadButton::Options => Key::BTN_1,
-            GamepadButton::Create => Key::BTN_0,
-            GamepadButton::Ps => Key::BTN_4, // Or maybe swap with mute?
-            GamepadButton::Mute => Key::BTN_5,
-            GamepadButton::BumperLeft => Key::BTN_DPAD_UP, // What?
-            GamepadButton::BumperRight => Key::BTN_DPAD_DOWN, // I don't even know…
-            GamepadButton::StickLeft => Key::BTN_2,
-            GamepadButton::StickRight => Key::BTN_3,
-            GamepadButton::Touchpad => todo!(),
+            GamepadButton::A => Key::BTN_SOUTH,
+            GamepadButton::B => Key::BTN_EAST,
+            GamepadButton::Y => Key::BTN_WEST,
+            GamepadButton::X => Key::BTN_NORTH,
+            GamepadButton::Start => Key::BTN_START,
+            GamepadButton::Select => Key::BTN_SELECT,
+            GamepadButton::Mode => Key::BTN_MODE,
+            GamepadButton::BumperLeft => Key::BTN_TL,
+            GamepadButton::BumperRight => Key::BTN_TR,
+            GamepadButton::StickLeft => Key::BTN_THUMBL,
+            GamepadButton::StickRight => Key::BTN_THUMBR,
+            GamepadButton::Down => Key::BTN_TRIGGER_HAPPY4,
+            GamepadButton::Right => Key::BTN_TRIGGER_HAPPY2,
+            GamepadButton::Up => Key::BTN_TRIGGER_HAPPY3,
+            GamepadButton::Left => Key::BTN_TRIGGER_HAPPY1,
         }
     }
 }
