54b0dfba99fa6d23dd0ea6c3b15aaf79f8b21e69
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2025-07-31 23:26:02 +0200

    Add example crate.

---
+++ example/examples/01-mouse-movement.rs
@@ -0,0 +1,23 @@
+use std::{io::Write, net::TcpStream, thread::sleep, time::Duration};
+
+use io_core::{DEFAULT_PORT, builtin::messages::mouse::Mouse, dispatcher::message::MessageEncoder};
+
+fn main() {
+    let addr = format!("0.0.0.0:{DEFAULT_PORT}");
+    let mut conn = TcpStream::connect(addr).unwrap();
+
+    let mut mouse = Mouse::default();
+    let mut delta = 0f32;
+    const RANGE: f32 = 10f32;
+    const STEP: f32 = 0.05;
+
+    loop {
+        mouse.0 = delta.cos() * RANGE;
+        mouse.1 = delta.sin() * RANGE;
+
+        conn.write(&MessageEncoder::encode(mouse)).unwrap();
+
+        delta = delta + STEP;
+        sleep(Duration::from_millis(4));
+    }
+}
